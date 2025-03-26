840bde2cc5d0cb9bd90878ecc35832660faea738
rrsarv <164464541+rrsarv@users.noreply.github.com>
2025-03-26 18:43:02 +0300

    add

--- my-rent-service/src/components/App/app.tsx
+++ my-rent-service/src/components/App/app.tsx
@@ -5,7 +5,9 @@ import FavoritesPage from "../../pages/favorites-page/favorites-page";
 import ErrorPage from "../../pages/error-page/error-page";
 import OfferPage from "../../pages/offer-page/offer-page";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-
+import { AppRoute} from "../../const";
+import { PrivateRoute } from "../private-route/private-route";
+import { AutorizationStatus } from "../../const";
 type AppMainPageProps={
     rentalOffersCount: number;
 }
@@ -15,10 +17,17 @@ function App({rentalOffersCount}: AppMainPageProps): JSX.Element{
   return(
     <Router>
       <Routes>
-        <Route path="/" element={<MainPage rentalOffersCount={rentalOffersCount} />} />
-        <Route path="/login" element={<LoginPage/>} />
-        <Route path="/offer" element={<OfferPage/>} />
-        <Route path="/favorites" element={<FavoritesPage/>} />
+        <Route path={AppRoute.Main} element={<MainPage rentalOffersCount={rentalOffersCount} />} />
+        <Route path={AppRoute.Login} element={<LoginPage/>} />
+        <Route path={AppRoute.Offer} element={<OfferPage/>} />
+        <Route path={AppRoute.Favorites} 
+          element={
+            <PrivateRoute
+              autorizationStatus={AutorizationStatus.NoAuth}>
+                <FavoritesPage/>
+            </PrivateRoute>
+            } 
+          />
         <Route path="*" element={<ErrorPage/>} />
       </Routes>
     </Router>
---
+++ my-rent-service/src/components/main-cities/main-cities.tsx
@@ -0,0 +1,45 @@
+import { JSX } from "react"
+import CitiesCard from "../cities-card/cities-card";
+type NearCitiesProps ={
+    rentalOffersCount: number;
+  }
+  
+function NearCities({rentalOffersCount}: NearCitiesProps): JSX.Element {
+    return(
+        <div className="cities">
+          <div className="cities__places-container container">
+            <section className="cities__places places">
+              <h2 className="visually-hidden">Places</h2>
+              <b className="places__found">{rentalOffersCount} places to stay in Amsterdam</b>
+              <form className="places__sorting" action="#" method="get">
+                <span className="places__sorting-caption">Sort by</span>
+                <span className="places__sorting-type" tabIndex={0}>
+                  Popular
+                  <svg className="places__sorting-arrow" width="7" height="4">
+                    <use href="#icon-arrow-select"></use>
+                  </svg>
+                </span>
+                <ul className="places__options places__options--custom places__options--opened">
+                  <li className="places__option places__option--active" tabIndex={0}>Popular</li>
+                  <li className="places__option" tabIndex={0}>Price: low to high</li>
+                  <li className="places__option" tabIndex={0}>Price: high to low</li>
+                  <li className="places__option" tabIndex={0}>Top rated first</li>
+                </ul>
+              </form>
+              <div className="cities__places-list places__list tabs__content">
+              <CitiesCard/>
+              <CitiesCard/>
+              <CitiesCard/>
+              <CitiesCard/>
+              <CitiesCard/>
+              </div>
+            </section>
+            <div className="cities__right-section">
+              <section className="cities__map map"></section>
+            </div>
+          </div>
+        </div>
+    )
+}
+
+export default NearCities;
