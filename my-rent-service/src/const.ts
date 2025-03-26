b15501461d75cec794c0bb5afe8020f870851d0c
rrsarv <164464541+rrsarv@users.noreply.github.com>
2025-03-26 18:37:27 +0300

    add

--- my-rent-service/src/const.ts
+++ my-rent-service/src/const.ts
@@ -2,4 +2,20 @@ const Setting= {
     rentalOffersCount:312,
 } as const;
 
+const AppRoute ={
+    Main : '/',
+    Login : '/login',
+    Favorites : '/favorites',
+    Offer : '/offer/id',
+} as const;
+
+const AutorizationStatus = {
+    Auth : 'AUTH',
+    NoAuth : 'NO_AUTH',
+    Unknown : 'UNKNOWN'
+}
+
+
 export { Setting};
+export {AppRoute};
+export {AutorizationStatus}
