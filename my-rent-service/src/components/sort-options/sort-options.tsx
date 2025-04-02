62aab5bea1a5e946e923e3c68fc69131cffb78d9
rrsarv <164464541+rrsarv@users.noreply.github.com>
2025-04-02 23:51:40 +0300

    update sort-optios

--- my-rent-service/src/components/sort-options/sort-options.tsx
+++ my-rent-service/src/components/sort-options/sort-options.tsx
@@ -34,7 +34,7 @@ function SortOptions({activeSorting,onChange} : SortPalcesProps){
         <form className="places__sorting" action="#" method="get" onKeyDown={keyDownHandler}>
                 <span className="places__sorting-caption">Sort by</span>
                 <span className="places__sorting-type" tabIndex={0} onClick={typeClickHandler}>
-                  Popular
+                { activeSorting }
                   <svg className="places__sorting-arrow" width="7" height="4" style={iconStyle}>
                     <use href="#icon-arrow-select"></use>
                   </svg>
